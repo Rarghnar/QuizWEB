b1ab7e2b4ce30a43837ab2df2798d4b7b6ba4970
David Godoy Ayala <godoyayaladavid@gmail.com>
2020-10-21 17:22:11 -0300

    final

--- js/app.js
+++ js/app.js
@@ -4,167 +4,167 @@ function nombre(nombre) {
         document.getElementById("nombre").innerHTML = "Sin Nombre";
     }
     if (!nombre.localeCompare('Sy')) {
-        document.getElementById("nombre").innerHTML = "Maria Silbylla Merian";
+        document.getElementById("nombre").innerHTML = "Sy <br> Ciencias Naturales <br>Maria Silbylla Merian";
     }
     if (!nombre.localeCompare('Av')) {
-        document.getElementById("nombre").innerHTML = "Ángeles Alvariño";
+        document.getElementById("nombre").innerHTML = "Av <br> Ciencias Naturales <br>Ángeles Alvariño";
     }
     if (!nombre.localeCompare('Ba')) {
-        document.getElementById("nombre").innerHTML = "Florence Bascom";
+        document.getElementById("nombre").innerHTML = "Ba <br> Ciencias Naturales <br>Florence Bascom";
     }
     if (!nombre.localeCompare('Ca')) {
-        document.getElementById("nombre").innerHTML = "Rachel Carson";
+        document.getElementById("nombre").innerHTML = "Ca <br> Ciencias Naturales <br>Rachel Carson";
     }
     if (!nombre.localeCompare('Cv')) {
-        document.getElementById("nombre").innerHTML = "Josefina Castellvi";
+        document.getElementById("nombre").innerHTML = "Cv <br> Ciencias Naturales <br>Josefina Castellvi";
     }
     if (!nombre.localeCompare('Lh')) {
-        document.getElementById("nombre").innerHTML = "Inge Lehman";
+        document.getElementById("nombre").innerHTML = "Lh <br> Ciencias Naturales <br>Inge Lehman";
     }
     if (!nombre.localeCompare('Mh')) {
-        document.getElementById("nombre").innerHTML = "Wangari Maathai";
+        document.getElementById("nombre").innerHTML = "Mh <br> Ciencias Naturales <br>Wangari Maathai";
     }
     if (!nombre.localeCompare('St')) {
-        document.getElementById("nombre").innerHTML = "Marie Stopes";
+        document.getElementById("nombre").innerHTML = "St <br> Ciencias Naturales <br>Marie Stopes";
     }
     if (!nombre.localeCompare('Th')) {
-        document.getElementById("nombre").innerHTML = "Marie Tharp y Sylvia Earle";
+        document.getElementById("nombre").innerHTML = "Th <br> Ciencias Naturales <br>Marie Tharp y Sylvia Earle";
     }
     // -------------------Inventoras
     if (!nombre.localeCompare('Ay')) {
-        document.getElementById("nombre").innerHTML = "Hertha Ayrton";
+        document.getElementById("nombre").innerHTML = "Ay <br> Inventora/Ingeniera <br>Hertha Ayrton";
     }
     if (!nombre.localeCompare('Cb')) {
-        document.getElementById("nombre").innerHTML = "Pilar Carbonero";
+        document.getElementById("nombre").innerHTML = "Cb <br> Inventora/Ingeniera <br>Pilar Carbonero";
     }
     if (!nombre.localeCompare('Cr')) {
-        document.getElementById("nombre").innerHTML = "Josephine Cochrane";
+        document.getElementById("nombre").innerHTML = "Cr <br> Inventora/Ingeniera <br>Josephine Cochrane";
     }
     if (!nombre.localeCompare('Ha')) {
-        document.getElementById("nombre").innerHTML = "Margaret Hamilton";
+        document.getElementById("nombre").innerHTML = "Ha <br> Inventora/Ingeniera <br>Margaret Hamilton";
     }
     if (!nombre.localeCompare('Ho')) {
-        document.getElementById("nombre").innerHTML = "Grace Hopper";
+        document.getElementById("nombre").innerHTML = "Ho <br> Inventora/Ingeniera <br>Grace Hopper";
     }
     if (!nombre.localeCompare('K')) {
-        document.getElementById("nombre").innerHTML = "Stephanie Kwolek";
+        document.getElementById("nombre").innerHTML = "K <br> Inventora/Ingeniera <br>Stephanie Kwolek";
     }
     if (!nombre.localeCompare('La')) {
-        document.getElementById("nombre").innerHTML = "Hedy Lamarr";
+        document.getElementById("nombre").innerHTML = "La <br> Inventora/Ingeniera <br>Hedy Lamarr";
     }
     if (!nombre.localeCompare('Rr')) {
-        document.getElementById("nombre").innerHTML = "Ángela Ruiz Robles";
+        document.getElementById("nombre").innerHTML = "Rr <br> Inventora/Ingeniera <br>Ángela Ruiz Robles";
     }
     //----------------- matematicas
     if (!nombre.localeCompare('Ag')) {
-        document.getElementById("nombre").innerHTML = "Maria Agnessi";
+        document.getElementById("nombre").innerHTML = "Ag <br> Matemáticas <br>Maria Agnessi";
     }
     if (!nombre.localeCompare('Bs')) {
-        document.getElementById("nombre").innerHTML = "Laura Bassi";
+        document.getElementById("nombre").innerHTML = "Bs <br> Matemáticas <br>Laura Bassi";
     }
     if (!nombre.localeCompare('By')) {
-        document.getElementById("nombre").innerHTML = "Pilar Bayer";
+        document.getElementById("nombre").innerHTML = "By <br> Matemáticas <br>Pilar Bayer";
     }
     if (!nombre.localeCompare('C')) {
-        document.getElementById("nombre").innerHTML = "Emilie du Chátelet";
+        document.getElementById("nombre").innerHTML = "C <br> Matemáticas <br>Emilie du Chátelet";
     }
     if (!nombre.localeCompare('Cl')) {
-        document.getElementById("nombre").innerHTML = "M. Antónia Canals";
+        document.getElementById("nombre").innerHTML = "Cl <br> Matemáticas <br>M. Antónia Canals";
     }
     if (!nombre.localeCompare('Cs')) {
-        document.getElementById("nombre").innerHTML = "M. Andrea Casamayor";
+        document.getElementById("nombre").innerHTML = "Cs <br> Matemáticas <br>M. Andrea Casamayor";
     }
     if (!nombre.localeCompare('Ge')) {
-        document.getElementById("nombre").innerHTML = "Sophie Germain";
+        document.getElementById("nombre").innerHTML = "Ge <br> Matemáticas <br>Sophie Germain";
     }
     if (!nombre.localeCompare('Gv')) {
-        document.getElementById("nombre").innerHTML = "Evelyn Boyd Granville";
+        document.getElementById("nombre").innerHTML = "Gv <br> Matemáticas <br>Evelyn Boyd Granville";
     }
     if (!nombre.localeCompare('H')) {
-        document.getElementById("nombre").innerHTML = "Hipatia de Alejandria";
+        document.getElementById("nombre").innerHTML = "H <br> Matemáticas <br>Hipatia de Alejandria";
     }
     if (!nombre.localeCompare('Jh')) {
-        document.getElementById("nombre").innerHTML = "Katherine Johnson";
+        document.getElementById("nombre").innerHTML = "Jh <br> Matemáticas <br>Katherine Johnson";
     }
     if (!nombre.localeCompare('Ko')) {
-        document.getElementById("nombre").innerHTML = "Sofia Kovalevskaya";
+        document.getElementById("nombre").innerHTML = "Ko <br> Matemáticas <br>Sofia Kovalevskaya";
     }
     if (!nombre.localeCompare('Lo')) {
-        document.getElementById("nombre").innerHTML = "Ada Lovelace";
+        document.getElementById("nombre").innerHTML = "Lo <br> Matemáticas <br>Ada Lovelace";
     }
     if (!nombre.localeCompare('Lp')) {
-        document.getElementById("nombre").innerHTML = "Nicole-Reine de Lepaute";
+        document.getElementById("nombre").innerHTML = "Lp <br> Matemáticas <br>Nicole-Reine de Lepaute";
     }
     if (!nombre.localeCompare('Mz')) {
-        document.getElementById("nombre").innerHTML = "Maryam Mirzajani";
+        document.getElementById("nombre").innerHTML = "Mz <br> Matemáticas <br>Maryam Mirzajani";
     }
     if (!nombre.localeCompare('No')) {
-        document.getElementById("nombre").innerHTML = "Emmy Noether";
+        document.getElementById("nombre").innerHTML = "No <br> Matemáticas <br>Emmy Noether";
     }
     if (!nombre.localeCompare('Sm')) {
-        document.getElementById("nombre").innerHTML = "Mary Sommerville";
+        document.getElementById("nombre").innerHTML = "Sm <br> Matemáticas <br>Mary Sommerville";
     }
     if (!nombre.localeCompare('Wo')) {
-        document.getElementById("nombre").innerHTML = "Maria Wonenburger";
+        document.getElementById("nombre").innerHTML = "Wo <br> Matemáticas <br>Maria Wonenburger";
     }
     // ------------------  Paleontologas
     if (!nombre.localeCompare('An')) {
-        document.getElementById("nombre").innerHTML = "Mary Anning";
+        document.getElementById("nombre").innerHTML = "An <br> Paleontóloga <br>Mary Anning";
     }
     if (!nombre.localeCompare('Lk')) {
-        document.getElementById("nombre").innerHTML = "Mary Leakey";
+        document.getElementById("nombre").innerHTML = "Lk <br> Paleontóloga <br>Mary Leakey";
     }
     if (!nombre.localeCompare('Ma')) {
-        document.getElementById("nombre").innerHTML = "Maria Martinón Torres";
+        document.getElementById("nombre").innerHTML = "Ma <br> Paleontóloga <br>Maria Martinón Torres";
     }
     //------------------------ Quimicas
     if (!nombre.localeCompare('Al')) {
-        document.getElementById("nombre").innerHTML = "Frances H. Amold";
+        document.getElementById("nombre").innerHTML = "Al <br> Químicas <br>Frances H. Amold";
     }
     if (!nombre.localeCompare('Bb')) {
-        document.getElementById("nombre").innerHTML = "Katharine Burr Blodgett";
+        document.getElementById("nombre").innerHTML = "Bb <br> Químicas <br>Katharine Burr Blodgett";
     }
     if (!nombre.localeCompare('Bn')) {
-        document.getElementById("nombre").innerHTML = "Dorotea Barnés y las químicas españolas de la edad de plata";
+        document.getElementById("nombre").innerHTML = "Bn <br> Químicas <br>Dorotea Barnés y las químicas españolas de la edad de plata";
     }
     if (!nombre.localeCompare('Cw')) {
-        document.getElementById("nombre").innerHTML = "Dorothy Crowfoot Hodgkin";
+        document.getElementById("nombre").innerHTML = "Cw <br> Químicas <br>Dorothy Crowfoot Hodgkin";
     }
     if (!nombre.localeCompare('Fr')) {
-        document.getElementById("nombre").innerHTML = "Rosalind Franklin";
+        document.getElementById("nombre").innerHTML = "Fr <br> Químicas <br>Rosalind Franklin";
     }
     if (!nombre.localeCompare('Fu')) {
-        document.getElementById("nombre").innerHTML = "Gertrudis de la Fuente";
+        document.getElementById("nombre").innerHTML = "Fu <br> Químicas <br>Gertrudis de la Fuente";
     }
     if (!nombre.localeCompare('Ju')) {
-        document.getElementById("nombre").innerHTML = "Manuela Juárez";
+        document.getElementById("nombre").innerHTML = "Ju <br> Químicas <br>Manuela Juárez";
     }
     if (!nombre.localeCompare('Ln')) {
-        document.getElementById("nombre").innerHTML = "Kathleen Lonsdale";
+        document.getElementById("nombre").innerHTML = "Ln <br> Químicas <br>Kathleen Lonsdale";
     }
     if (!nombre.localeCompare('Me')) {
-        document.getElementById("nombre").innerHTML = "Marie Meurdrac & Jane Marcet";
+        document.getElementById("nombre").innerHTML = "Me <br> Químicas <br>Marie Meurdrac & Jane Marcet";
     }
     if (!nombre.localeCompare('Mn')) {
-        document.getElementById("nombre").innerHTML = "Rosa M. Menédez";
+        document.getElementById("nombre").innerHTML = "Mn <br> Químicas <br>Rosa M. Menédez";
     }
     if (!nombre.localeCompare('Nd')) {
-        document.getElementById("nombre").innerHTML = "Ida Nodaack";
+        document.getElementById("nombre").innerHTML = "Nd <br> Químicas <br>Ida Nodaack";
     }
     if (!nombre.localeCompare('Pc')) {
-        document.getElementById("nombre").innerHTML = "Agnes Pockels";
+        document.getElementById("nombre").innerHTML = "Pc <br> Químicas <br>Agnes Pockels";
     }
     if (!nombre.localeCompare('Pz')) {
-        document.getElementById("nombre").innerHTML = "Marie Anne Paulze";
+        document.getElementById("nombre").innerHTML = "Pz <br> Químicas <br>Marie Anne Paulze";
     }
     if (!nombre.localeCompare('Rc')) {
-        document.getElementById("nombre").innerHTML = "Eilen Richards";
+        document.getElementById("nombre").innerHTML = "Rc <br> Químicas <br>Eilen Richards";
     }
     if (!nombre.localeCompare('Yn')) {
-        document.getElementById("nombre").innerHTML = "Ada Yonath";
+        document.getElementById("nombre").innerHTML = "Yn <br> Químicas <br>Ada Yonath";
     }
     //-------------------------- Primatologas
     if (!nombre.localeCompare('Gd')) {
-        document.getElementById("nombre").innerHTML = "Jane Goodall, Dian Fossey & Biruté Galdikas";
+        document.getElementById("nombre").innerHTML = "Gd <br> Primatóloga <br>Jane Goodall, Dian Fossey & Biruté Galdikas";
     }
 }
