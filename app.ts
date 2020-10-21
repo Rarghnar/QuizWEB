51f4d073dcb5cf589c0041b4599b57ed7e5a16c2
David Godoy Ayala <godoyayaladavid@gmail.com>
2020-10-21 16:38:15 -0300

    Control prueba

---
+++ app.ts
@@ -0,0 +1,183 @@
+function nombre(nombre: string) {  
+  
+  if (nombre.localeCompare('')) {
+    document.getElementById("nombre")!.innerHTML = "Sin Nombre";
+  }
+  if (!nombre.localeCompare('Sy')) {
+    document.getElementById("nombre")!.innerHTML = "Maria Silbylla Merian";
+  }
+  if (!nombre.localeCompare('Av')) {
+    document.getElementById("nombre")!.innerHTML = "Ángeles Alvariño";
+  }
+  if (!nombre.localeCompare('Ba')) {
+    document.getElementById("nombre")!.innerHTML = "Florence Bascom";
+  }
+  if (!nombre.localeCompare('Ca')) {
+    document.getElementById("nombre")!.innerHTML = "Rachel Carson";
+  }
+  if (!nombre.localeCompare('Cv')) {
+    document.getElementById("nombre")!.innerHTML = "Josefina Castellvi";
+  }
+  if (!nombre.localeCompare('Lh')) {
+    document.getElementById("nombre")!.innerHTML = "Inge Lehman";
+  }
+  if (!nombre.localeCompare('Mh')) {
+    document.getElementById("nombre")!.innerHTML = "Wangari Maathai";
+  }
+  if (!nombre.localeCompare('St')) {
+    document.getElementById("nombre")!.innerHTML = "Marie Stopes";
+  }
+  if (!nombre.localeCompare('Th')) {
+    document.getElementById("nombre")!.innerHTML = "Marie Tharp y Sylvia Earle";
+  }
+
+  // -------------------Inventoras
+  if (!nombre.localeCompare('Ay')) {
+    document.getElementById("nombre")!.innerHTML = "Hertha Ayrton";
+  }
+  if (!nombre.localeCompare('Cb')) {
+    document.getElementById("nombre")!.innerHTML = "Pilar Carbonero";
+  }
+  if (!nombre.localeCompare('Cr')) {
+    document.getElementById("nombre")!.innerHTML = "Josephine Cochrane";
+  }
+  if (!nombre.localeCompare('Ha')) {
+    document.getElementById("nombre")!.innerHTML = "Margaret Hamilton";
+  }
+  if (!nombre.localeCompare('Ho')) {
+    document.getElementById("nombre")!.innerHTML = "Grace Hopper";
+  }
+  if (!nombre.localeCompare('K')) {
+    document.getElementById("nombre")!.innerHTML = "Stephanie Kwolek";
+  }
+  if (!nombre.localeCompare('La')) {
+    document.getElementById("nombre")!.innerHTML = "Hedy Lamarr";
+  }
+  if (!nombre.localeCompare('Rr')) {
+    document.getElementById("nombre")!.innerHTML = "Ángela Ruiz Robles";
+  }
+
+  //----------------- matematicas
+
+  if (!nombre.localeCompare('Ag')) {
+    document.getElementById("nombre")!.innerHTML = "Maria Agnessi";
+  }
+  if (!nombre.localeCompare('Bs')) {
+    document.getElementById("nombre")!.innerHTML = "Laura Bassi";
+  }
+  if (!nombre.localeCompare('By')) {
+    document.getElementById("nombre")!.innerHTML = "Pilar Bayer";
+  }
+  if (!nombre.localeCompare('C')) {
+    document.getElementById("nombre")!.innerHTML = "Emilie du Chátelet";
+  }
+  if (!nombre.localeCompare('Cl')) {
+    document.getElementById("nombre")!.innerHTML = "M. Antónia Canals";
+  }
+  if (!nombre.localeCompare('Cs')) {
+    document.getElementById("nombre")!.innerHTML = "M. Andrea Casamayor";
+  }
+  if (!nombre.localeCompare('Ge')) {
+    document.getElementById("nombre")!.innerHTML = "Sophie Germain";
+  }
+  if (!nombre.localeCompare('Gv')) {
+    document.getElementById("nombre")!.innerHTML = "Evelyn Boyd Granville";
+  }
+  if (!nombre.localeCompare('H')) {
+    document.getElementById("nombre")!.innerHTML = "Hipatia de Alejandria";
+  }
+  if (!nombre.localeCompare('Jh')) {
+    document.getElementById("nombre")!.innerHTML = "Katherine Johnson";
+  }
+  if (!nombre.localeCompare('Ko')) {
+    document.getElementById("nombre")!.innerHTML = "Sofia Kovalevskaya";
+  }
+  if (!nombre.localeCompare('Lo')) {
+    document.getElementById("nombre")!.innerHTML = "Ada Lovelace";
+  }
+  if (!nombre.localeCompare('Lp')) {
+    document.getElementById("nombre")!.innerHTML = "Nicole-Reine de Lepaute";
+  }
+  if (!nombre.localeCompare('Mz')) {
+    document.getElementById("nombre")!.innerHTML = "Maryam Mirzajani";
+  }
+  if (!nombre.localeCompare('No')) {
+    document.getElementById("nombre")!.innerHTML = "Emmy Noether";
+  }
+  if (!nombre.localeCompare('Sm')) {
+    document.getElementById("nombre")!.innerHTML = "Mary Sommerville";
+  }
+  if (!nombre.localeCompare('Wo')) {
+    document.getElementById("nombre")!.innerHTML = "Maria Wonenburger";
+  }
+
+  // ------------------  Paleontologas
+
+  if (!nombre.localeCompare('An')) {
+    document.getElementById("nombre")!.innerHTML = "Mary Anning";
+  }
+  if (!nombre.localeCompare('Lk')) {
+    document.getElementById("nombre")!.innerHTML = "Mary Leakey";
+  }
+  if (!nombre.localeCompare('Ma')) {
+    document.getElementById("nombre")!.innerHTML = "Maria Martinón Torres";
+  }
+
+  //------------------------ Quimicas
+
+  if (!nombre.localeCompare('Al')) {
+    document.getElementById("nombre")!.innerHTML = "Frances H. Amold";
+  }
+  if (!nombre.localeCompare('Bb')) {
+    document.getElementById("nombre")!.innerHTML = "Katharine Burr Blodgett";
+  }
+  if (!nombre.localeCompare('Bn')) {
+    document.getElementById("nombre")!.innerHTML = "Dorotea Barnés y las químicas españolas de la edad de plata";
+  }
+  if (!nombre.localeCompare('Cw')) {
+    document.getElementById("nombre")!.innerHTML = "Dorothy Crowfoot Hodgkin";
+  }
+  if (!nombre.localeCompare('Fr')) {
+    document.getElementById("nombre")!.innerHTML = "Rosalind Franklin";
+  }
+  if (!nombre.localeCompare('Fu')) {
+    document.getElementById("nombre")!.innerHTML = "Gertrudis de la Fuente";
+  }
+  if (!nombre.localeCompare('Ju')) {
+    document.getElementById("nombre")!.innerHTML = "Manuela Juárez";
+  }
+  if (!nombre.localeCompare('Ln')) {
+    document.getElementById("nombre")!.innerHTML = "Kathleen Lonsdale";
+  }
+  if (!nombre.localeCompare('Me')) {
+    document.getElementById("nombre")!.innerHTML = "Marie Meurdrac & Jane Marcet";
+  }
+  if (!nombre.localeCompare('Mn')) {
+    document.getElementById("nombre")!.innerHTML = "Rosa M. Menédez";
+  }
+  if (!nombre.localeCompare('Nd')) {
+    document.getElementById("nombre")!.innerHTML = "Ida Nodaack";
+  }
+  if (!nombre.localeCompare('Pc')) {
+    document.getElementById("nombre")!.innerHTML = "Agnes Pockels";
+  }
+  if (!nombre.localeCompare('Pz')) {
+    document.getElementById("nombre")!.innerHTML = "Marie Anne Paulze";
+  }
+  if (!nombre.localeCompare('Rc')) {
+    document.getElementById("nombre")!.innerHTML = "Eilen Richards";
+  }
+  if (!nombre.localeCompare('Yn')) {
+    document.getElementById("nombre")!.innerHTML = "Ada Yonath";
+  }
+
+  //-------------------------- Primatologas
+
+  if (!nombre.localeCompare('Gd')) {
+    document.getElementById("nombre")!.innerHTML = "Jane Goodall, Dian Fossey & Biruté Galdikas";
+  }
+
+}
+  
+  
+
